59df4219f695a3df8363f8e4b05b8d46ee76fa6b
mikelee005 <mikelee005@gmail.com>
2017-04-03 20:24:00 -0700

    AddressEnumTypes.cs updated

--- AddressEnumType.cs
+++ AddressEnumType.cs
@@ -1,55 +1,17 @@
-using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Net.Http.Formatting;
-using System.Net.Http.Headers;
-using System.Web.Http;
+using System.Web;
 
-namespace adr.Web
+namespace Sabio.Web.Enums
 {
-    public static class WebApiConfig
+    public enum AddressType
     {
-        public static void Register(HttpConfiguration config)
-        {
-            // Web API configuration and services
 
-            /* adrMVC: Attribute routing is in View Controllers are enabled by the MapHttpAttributeRoutes call
-              * this line finds the Controllers in the application, then it reads the Route Attributes and 
-              * puts the appropriate data in the route table.
-              * 
-              * this route table is what is searched by the frame work to determine what to end point to use
-             
-              */
-            config.MapHttpAttributeRoutes();
+        Primary = 1,
+        Office = 2,
+        Warehouse = 3,
+        ProjectSite = 4
 
-            //adrMVC: this is the direct way to put something in the route table
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-
-
-            //adr: we remove the xml serilizer to make life easier. 
-            MediaTypeHeaderValue appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
-            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
-
-            var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
-
-            //adrWebApi: this is what transforms all the Json into camelcase for us
-            json.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-
-            //- 1/11/2017 edit
-            //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-            JsonMediaTypeFormatter jsonFormatter = new JsonMediaTypeFormatter();
-            //optional: set serializer settings here
-            //config.Services.Replace(typeof(IContentNegotiator), new JsonContentNegotiator(jsonFormatter));
-
-            MediaTypeFormatterCollection formatters = config.Formatters;
-            JsonMediaTypeFormatter jFormatter = formatters.JsonFormatter;
-            JsonSerializerSettings settings = jsonFormatter.SerializerSettings;
-            // settings.Formatting = Formatting.Indented;
-            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-        }
     }
 }
